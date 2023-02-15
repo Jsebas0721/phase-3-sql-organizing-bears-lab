233caa2d2f42aa08d14fe55c67b3b73608020be5
sebastian medina <sebastianlopez_21@hotmail.com>
2023-02-15 11:39:19 -0500

    commit tree f6d16d768225218bcdfcfac5e2b15484d2687120

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 3, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", 4, "F", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Tabitha", 1, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 4, "F", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Melissa", 3, "F", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Grinch", 2, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Wendy", 2, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 5, "M", "Brown", "Calm", 1);
